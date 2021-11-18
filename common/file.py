e86099bc2a6c443cdced502b1770f1587869622c
ravielze <ravielz20@gmail.com>
2021-11-18 21:30:27 +0700

    Add file class

---
+++ common/file.py
@@ -0,0 +1,19 @@
+from math import ceil
+
+
+class File:
+    def __init__(self, path: str):
+        assert path != None
+        assert type(path) is str
+        assert len(path) > 0
+
+        self.path = path
+
+    def filesize(self):
+        with open(self.path, 'rb') as file:
+            file.seek(0, 2)
+            return file.tell()
+
+    def countSegment(self):
+        fileSize = self.filesize()
+        return ceil(fileSize / 32768)
